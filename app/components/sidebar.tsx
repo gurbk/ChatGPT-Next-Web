7782ebc49471ea5229ab761e7520627b6bb69967
你好呀 <111191349+88lin@users.noreply.github.com>
2023-04-21 12:30:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -86,7 +86,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>故人的ChatGPT小助手</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
