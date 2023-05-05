887950cc69894b4269e3107e1f3cee1795c71eec
你好呀 <111191349+88lin@users.noreply.github.com>
2023-05-06 01:10:15 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,9 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>故人的ChatGPT小助手</div>
         <div className={styles["sidebar-sub-title"]}>
-        官方限制是3条/min，可以一次性发完所有提问 每人每天0.2就能运营下去<a href="https://520.txsb.gq/fireworks/1.html" target="_blank" rel="noopener noreferrer">
+        官方限制是3条/min，可以一次性发完所有提问 
+        已捐赠用户联系wx：guren_222 获取独立key
+        每人每天0.2就能运营下去<a href="https://520.txsb.gq/fireworks/1.html" target="_blank" rel="noopener noreferrer">
             <span className={styles["sidebar-sub-title-gradient"]}>欢迎捐赠</span>
             </a>
         </div>
