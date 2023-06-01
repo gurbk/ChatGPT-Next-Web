feb1f71c951f55d63f5aece1e60df551e6b053d3
你好呀 <111191349+88lin@users.noreply.github.com>
2023-06-01 20:44:14 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,7 +119,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>故人的AI助理</div>
+        <div className={styles["sidebar-title"]}>小林的AI助理</div>
         <div className={styles["sidebar-sub-title"]}>
           
         </div>
