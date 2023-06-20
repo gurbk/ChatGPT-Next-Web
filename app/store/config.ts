2125b5d828367887c61b880fb06edb140cba0bdf
你好呀 <111191349+88lin@users.noreply.github.com>
2023-06-20 20:59:12 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -31,10 +31,10 @@ export const DEFAULT_CONFIG = {
   dontShowMaskSplashScreen: false, // dont show splash screen when create chat
 
   modelConfig: {
-    model: "gpt-3.5-turbo" as ModelType,
-    temperature: 0.5,
-    max_tokens: 2000,
-    presence_penalty: 0,
+    model: "gpt-3.5-turbo-16k" as ModelType,
+    temperature: 0,
+    max_tokens: 32000,
+    presence_penalty: 2,
     frequency_penalty: 0,
     sendMemory: true,
     historyMessageCount: 6,
