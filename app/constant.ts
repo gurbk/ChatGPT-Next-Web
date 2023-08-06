89db7355ab765e8222a8abe1915447c180f5dcd2
你好呀 <111191349+88lin@users.noreply.github.com>
2023-08-06 13:26:40 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -2,7 +2,7 @@ export const OWNER = "88lin";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `https://github.com/Yidadaa/ChatGPT-Next-Web`;
+export const UPDATE_URL = `https://github.com/Yidadaa/ChatGPT-Next-Web/#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
